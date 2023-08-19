9ceff9ebc175124762c79f630af135f2b8fceb1d
Mattioz <61207452+Mattioz@users.noreply.github.com>
2023-08-18 21:09:17 -0300

    Add Score

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -24,9 +24,14 @@ public class Quiz : MonoBehaviour
     [Header("Timer")]
     [SerializeField] Image timerImage;
     Timer timer;
+
+    [Header("Scoring")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
     void Start()
     {   
         timer = FindAnyObjectByType<Timer>();
+        scoreKeeper = FindAnyObjectByType<ScoreKeeper>();
     }
 
     void Update()
@@ -50,9 +55,9 @@ public class Quiz : MonoBehaviour
     {
         hasAnsweredEarly = true;
         DisplayAnswer(index);
-
         SetButtonState(false);
         timer.CancelTimer();
+        scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
     }
 
     void DisplayAnswer(int index)
@@ -64,6 +69,7 @@ public class Quiz : MonoBehaviour
             questionText.text = "Correct !!!";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
 
         } 
         else 
@@ -84,6 +90,7 @@ public class Quiz : MonoBehaviour
             SetDefaultButtonSprite();
             GetRandomQuestion();
             DisplayQuestion();
+            scoreKeeper.IncrementQuestionsSeen();
         }
     }
 
